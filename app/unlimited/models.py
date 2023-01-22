0ac17ac7253a27382b492e9ce9bfa6bb327b2939
MylesACD <49326725+MylesACD@users.noreply.github.com>
2023-01-21 22:35:55 -0800

    removed try on character image save

--- app/unlimited/models.py
+++ app/unlimited/models.py
@@ -31,15 +31,13 @@ class Character(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         
-        try:
+      
             img = Image.open(self.image.path)
             if img.height>300 or img.width>300:
                 output_size = (300,300)
                 img.thumbnail(output_size)
                 img.save(self.image.path)
-        except:
-            pass
-            
+      
         return  super().save(*args,**kwargs)
     
     def get_absolute_url(self):
