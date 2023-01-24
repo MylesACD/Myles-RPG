e90aff9e0fda32df87719139e27628fa93b29ff2
MylesACD <49326725+MylesACD@users.noreply.github.com>
2023-01-24 11:52:21 -0800

    character image field

--- app/unlimited/forms.py
+++ app/unlimited/forms.py
@@ -50,6 +50,9 @@ class CharacterForm(forms.ModelForm):
     class Meta:
         model = Character
         fields=["name","level","image",]
+        widgets = {
+            'image': forms.FileInput
+        }
     
     def __init__(self,*args,**kwargs):
         super(CharacterForm, self).__init__(*args, **kwargs)
