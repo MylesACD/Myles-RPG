83daafb3a825230316114dc53f4b428e4f0be237
MylesACD <49326725+MylesACD@users.noreply.github.com>
2023-01-24 12:15:26 -0800

    redirect editing

--- app/Myles-RPG/settings.py
+++ app/Myles-RPG/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # redirect to home page
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/unlimited"
 LOGIN_URL = "login"
 
 #TODO password reset using emails is very annoying
